745b379f883a71680f6f71d3afbb5c9254cf4e0c
Abhishek Singh <abhip13@gmail.com.com>
2023-10-22 20:26:47 +0530

    second commit

--- Login/script.js
+++ Login/script.js
@@ -1,27 +1,27 @@
-const userModel = require("../../src/models/register");
+// const userModel = require("../../src/models/register");
 
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-// Connection URI
-async function onFinishHandler(data) {
-try {
-await client.connect();
-const database = client.db('test');
-const collection = database.collection('registers');
+// // Connection URI
+// async function onFinishHandler(data) {
+// try {
+// await client.connect();
+// const database = client.db('test');
+// const collection = database.collection('registers');
 
-// Insert data into the collection
-const result = await collection.insertOne(data);
-console.log(`A document was inserted with the _id: ${result.insertedId}`);
-} finally {
-// Ensures that the client will close when you finish/error
-await client.close();
-}
-}
+// // Insert data into the collection
+// const result = await collection.insertOne(data);
+// console.log(`A document was inserted with the _id: ${result.insertedId}`);
+// } finally {
+// // Ensures that the client will close when you finish/error
+// await client.close();
+// }
+// }
 
-// Call the onFinishHandler with the data you want to store
-const data = {
-email: 'example@example.com', // Replace with the actual email from the form
-password: 'example123', // Replace with the actual password from the form
-};
+// // Call the onFinishHandler with the data you want to store
+// const data = {
+// email: 'example@example.com', // Replace with the actual email from the form
+// password: 'example123', // Replace with the actual password from the form
+// };
 
-onFinishHandler(data);
+// onFinishHandler(data);
